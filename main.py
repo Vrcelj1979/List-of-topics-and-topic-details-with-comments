d15930de5278b43568afb7d65f9c40bd46066f1e
Slobodan Vrcelj <slobodan.vrcelj79@gmail.com>
2018-11-19 16:31:48 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import webapp2
+from handlers.base import CookieAlertHandler, MainHandler
+from handlers.topics import TopicAdd, TopicDetails, TitleTopicHandler
+from handlers.comment import CommentAdd
+
+
+app = webapp2.WSGIApplication([
+    webapp2.Route('/', MainHandler, name="main-page"),
+    webapp2.Route('/set-cookie', CookieAlertHandler, name="set-cookie"),
+    webapp2.Route('/topic/add', TopicAdd, name="topic-add"),
+    webapp2.Route('/title', TitleTopicHandler, name="title"),
+    webapp2.Route('/topic/<topic_id:\d+>', TopicDetails, name="topic-details"),
+    webapp2.Route('/topic/<topic_id:\d+>/comment/add', CommentAdd, name="comment-add"),
+], debug=True)
